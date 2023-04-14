c05bd5690e6f68c1fe2ccae9ad888111a16edba1
ZincCat <zinccloride@outlook.com>
2023-04-14 16:39:10 +0000

    changed save embedding

--- embedding_save.py
+++ embedding_save.py
@@ -1,14 +1,17 @@
 import openai
 import numpy as np
+import os
 
-def embedding(output, embedding_model="text-embedding-ada-002"):
+def save_embedding(output, path, name, embedding_model="text-embedding-ada-002"):
     response = openai.Embedding.create(
         input=output,
-        model=embedding_model
+        model=embedding_model,
+        
     )
     output_embedding = np.array(response["data"][0]["embedding"])
     # save to disk
-    np.save("embedding.npy", output_embedding)
+    np.save(os.path.join(path, "{}.npy".format(name)), output_embedding)
     
-
-embedding("Revise an academic paragraph for clarity and conciseness without changing its meaning.")
+path = './'
+name = 'embedding'
+save_embedding("Revise an academic paragraph for clarity and conciseness without changing its meaning.", path, name)
